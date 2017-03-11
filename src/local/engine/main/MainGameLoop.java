c97f93e91ea8be30c179b66352484fbbd034af2e
Tim Vögtli <tim.voegtli@gibmit.ch>
2017-03-11 12:08:13 +0100

    work on OBJLoader

--- src/local/engine/main/MainGameLoop.java
+++ src/local/engine/main/MainGameLoop.java
@@ -12,6 +12,7 @@ import local.engine.renderes.Loader;
 import local.engine.renderes.Renderer;
 import local.engine.shaders.StaticShader;
 import local.engine.textures.ModelTexture;
+import local.engine.utilities.OBJLoader;
 
 public class MainGameLoop {
 	
@@ -19,95 +20,17 @@ public class MainGameLoop {
 		
 		DisplayManager.createDisplay();
 		Loader loader = new Loader();
+		
 		StaticShader shader = new StaticShader();
 		Renderer renderer = new Renderer( shader );
 		
-		float[] vertices = {			
-				-0.5f,0.5f,-0.5f,	
-				-0.5f,-0.5f,-0.5f,	
-				0.5f,-0.5f,-0.5f,	
-				0.5f,0.5f,-0.5f,		
-				
-				-0.5f,0.5f,0.5f,	
-				-0.5f,-0.5f,0.5f,	
-				0.5f,-0.5f,0.5f,	
-				0.5f,0.5f,0.5f,
-				
-				0.5f,0.5f,-0.5f,	
-				0.5f,-0.5f,-0.5f,	
-				0.5f,-0.5f,0.5f,	
-				0.5f,0.5f,0.5f,
-				
-				-0.5f,0.5f,-0.5f,	
-				-0.5f,-0.5f,-0.5f,	
-				-0.5f,-0.5f,0.5f,	
-				-0.5f,0.5f,0.5f,
-				
-				-0.5f,0.5f,0.5f,
-				-0.5f,0.5f,-0.5f,
-				0.5f,0.5f,-0.5f,
-				0.5f,0.5f,0.5f,
-				
-				-0.5f,-0.5f,0.5f,
-				-0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,0.5f
-				
-		};
-		
-		float[] textureCoords = {
-				
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0
-
-				
-		};
-		
-		int[] indices = {
-				0,1,3,	
-				3,1,2,	
-				4,5,7,
-				7,5,6,
-				8,9,11,
-				11,9,10,
-				12,13,15,
-				15,13,14,	
-				16,17,19,
-				19,17,18,
-				20,21,23,
-				23,21,22
-
-		};
-		
-		RawModel model = loader.loadToVAO( vertices, textureCoords, indices );
+		RawModel model = OBJLoader.loadObjModel( "test", loader );
 		ModelTexture texture = new ModelTexture( loader.loadTexture( "img" ) );
 		TexturedModel texturedModel = new TexturedModel( model, texture );
 		
 		Entity entity = new Entity(
 				texturedModel,
-				new Vector3f( 0, 0, -2 ),
+				new Vector3f( 0, -2, -20 ),
 				new Vector3f( 0, 0, 0 ),
 				new Vector3f( 1f, 1f, 1f )
 		);
@@ -119,7 +42,7 @@ public class MainGameLoop {
 		
 		while(!Display.isCloseRequested()){
 			
-			entity.increaseRotation( 1, 1, 0 );
+			entity.increaseRotation( 0, 0.5f, 0 );
 			camera.move();
 			
 			renderer.prepare();
--- src/local/engine/utilities/OBJLoader.java
+++ src/local/engine/utilities/OBJLoader.java
@@ -3,6 +3,7 @@ package local.engine.utilities;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.lwjgl.util.vector.Vector2f;
@@ -14,7 +15,7 @@ import local.engine.renderes.Loader;
 public class OBJLoader {
 	
 	private static List<Vector3f>	vertices	= new ArrayList<Vector3f>();
-	private static List<Vector2f>	testures	= new ArrayList<Vector2f>();
+	private static List<Vector2f>	textures	= new ArrayList<Vector2f>();
 	private static List<Vector3f>	normals		= new ArrayList<Vector3f>();
 	private static List<Integer>	indices 	= new ArrayList<Integer>();
 	
@@ -24,7 +25,7 @@ public class OBJLoader {
 	private static int[]	indicesArray	= null;
 	
 	public static RawModel loadObjModel(String fileName, Loader loader ){
-		fileName = "src/loacal/engine/res" + fileName + ".obj";
+		fileName = "src/local/engine/res/" + fileName + ".obj";
 		
 		
 		try(BufferedReader br = new BufferedReader(new FileReader( fileName ) ) ) {
@@ -43,7 +44,7 @@ public class OBJLoader {
 		    		
 		    	} else if( line.startsWith( "vt " ) ){
 		    		
-		    		testures.add(
+		    		textures.add(
 		    				new Vector2f(
 		    						Float.parseFloat( currentLine[ 1 ] ),
 		    						Float.parseFloat( currentLine[ 2 ] )
@@ -60,29 +61,51 @@ public class OBJLoader {
 		    				)
 		    		);
 		    		
-		    	} else if( line.contains( "s off" ) ){
+		    	} else if( line.contains( "s " ) ){
 		    		verticesArray	= new float[ vertices.size() * 3 ];
-		    		textureArray	= new float[ testures.size() * 2 ];
-		    		normalsArray	= new float[ normals.size() * 3 ];
+		    		textureArray	= new float[ vertices.size() * 2 ];
+		    		normalsArray	= new float[ vertices.size() * 3 ];
 		    		
 		    	} else if( line.startsWith( "f " ) ){
-		    		String[] vertex1 = currentLine[ 1 ].split( "/" );
-		    		String[] vertex2 = currentLine[ 2 ].split( "/" );
-		    		String[] vertex3 = currentLine[ 3 ].split( "/" );
-		    		processVertex( vertex1 );
+		    		processVertex( currentLine[ 1 ].split( "/" ) );
+		    		processVertex( currentLine[ 2 ].split( "/" ) );
+		    		processVertex( currentLine[ 3 ].split( "/" ) );
 		    	}
 		    }
-		    // line is not visible here.
 		} catch ( Exception e ){
 			e.printStackTrace();
 		}
+		
+		indicesArray = new int[ indices.size() ];
+		
+		for( int i = 0; i < indices.size(); i++ ){
+			indicesArray[ i ] = indices.get( i );
+		}
+		
+		for( int i = 0; i < vertices.size(); i++ ){
+			verticesArray[ i * 3 ]		= vertices.get( i ).x;
+			verticesArray[ i * 3 + 1 ]	= vertices.get( i ).y;
+			verticesArray[ i * 3 + 2 ]	= vertices.get( i ).z;
+		}
+		
 		
 		return loader.loadToVAO( verticesArray, textureArray, indicesArray );
 	}
 	
 	private static void processVertex( String[] vertexData ){
-		indices.add( Integer.parseInt( vertexData[1] )-1 );
-		System.out.println( indices.get(indices.size()-1 ) );
+		int currentVertexPointer	= Integer.parseInt( vertexData[0] ) - 1;
+		
+		Vector2f currentTex			= textures.get( Integer.parseInt( vertexData[1] ) - 1 );
+		Vector3f currentNorm		= normals.get( Integer.parseInt( vertexData[2] ) - 1 );
+		
+		indices.add( currentVertexPointer );
+		
+		textureArray[ currentVertexPointer * 2 ]		= currentTex.x;
+		textureArray[ currentVertexPointer * 2 + 1 ]	= 1 - currentTex.y;
+		
+		normalsArray[ currentVertexPointer * 3 ]		= currentNorm.x;
+		normalsArray[ currentVertexPointer * 3 + 1 ]	= currentNorm.y;
+		normalsArray[ currentVertexPointer * 3 + 2 ]	= currentNorm.z;
 	}
 
 }
